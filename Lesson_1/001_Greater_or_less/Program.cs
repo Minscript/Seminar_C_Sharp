46c5bfd33015658207d904d0595b833e9bbd99ba
Minscript <sretentsevmax@gmail.com>
2022-08-11 15:28:42 +0300

    Lesson_1 Complite

---
+++ Lesson_1/001_Greater_or_less/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+
+//Ввод данных пользователем
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Проверка данных с выводом ответа
+if (firstNumber>secondNumber)
+{
+    Console.Write($"Число {firstNumber} больше числа {secondNumber} ({firstNumber}>{secondNumber})");
+} else if (secondNumber>firstNumber)
+{
+    Console.Write($"Число {secondNumber} больше числа {firstNumber} ({secondNumber}>{firstNumber})");
+} else {
+    Console.Write($"Числа равны ({secondNumber}={firstNumber})");
+}
---
+++ Lesson_1/002_Maximum_of_three/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+
+//Ввод данных пользователем
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int lastNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+
+//Проверка данных с выводом ответа
+if (max==secondNumber && max==lastNumber)
+{
+    Console.Write("Введёные числа равны!");
+} else 
+{
+    if (max<secondNumber) {max=secondNumber;}
+    if (max<lastNumber) {max=lastNumber;}
+    Console.Write($"{max} является максимальным числом из чисел: {firstNumber}, {secondNumber}, {lastNumber}");
+}
