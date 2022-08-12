069b60ebef24e60e2e0b40cd751bd665e533c75e
Minscript <sretentsevmax@gmail.com>
2022-08-12 09:36:03 +0300

    Lesson_2 Complite update1

--- Lesson_2/007_Day_of_the_number/Program.cs
+++ Lesson_2/007_Day_of_the_number/Program.cs
@@ -5,32 +5,36 @@ Console.Clear();
 Console.Write("Введите любое число от 1 до 7: ");
 int week = Convert.ToInt32(Console.ReadLine());
 
-
 //Проверяем, является ли число днём недели и определяем по нему - выходной ли это день?
-Console.Write(week);
-switch (week){
-    case 1:
-        Console.Write(" - Рабочий день");
-        break;
-    case 2:
-        Console.Write(" - Рабочий день");
-        break;
-    case 3:
-        Console.Write(" - Рабочей недели");
-        break;
-    case 4:
-        Console.Write(" - Рабочий день");
-        break;
-    case 5:
-        Console.Write(" - Короткий рабочий день");
-        break;
-    case 6:
-        Console.Write(" - Выходной");
-        break;
-    case 7:
-        Console.Write(" - Выходной");
-        break;
-    default:
-        Console.Write(" - Не является числом обозначающий день недели!");
-        break;
-}
+void whatWeek(int num){
+    Console.Write(week);
+    switch (week){
+        case 1:
+            Console.Write(" - Рабочий день");
+            break;
+        case 2:
+            Console.Write(" - Рабочий день");
+            break;
+        case 3:
+            Console.Write(" - Рабочей недели");
+            break;
+        case 4:
+            Console.Write(" - Рабочий день");
+            break;
+        case 5:
+            Console.Write(" - Короткий рабочий день");
+            break;
+        case 6:
+            Console.Write(" - Выходной");
+            break;
+        case 7:
+            Console.Write(" - Выходной");
+            break;
+        default:
+            Console.Write(" - Не является числом обозначающий день недели!");
+            break;
+    }
+};
+
+//Выполняем "функцию"
+whatWeek(week);
