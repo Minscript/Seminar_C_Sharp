2b8ca0cc7f5fd003622d1f82c7ae1233f348df42
Minscript <sretentsevmax@gmail.com>
2022-08-12 09:28:02 +0300

    Lesson_2 Complite

---
+++ Lesson_2/005_The_second_number_of_three/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+//Создаём переменные
+Console.Write("Введите трехзначное число или 0 для генерации числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+
+//Проверяем ввод переменной и выполняем метод
+if (number==0)
+{
+    //Генерируем число за место пользователя, не забываем показать его.
+    Random random = new Random();
+    number = random.Next(100,1000);
+    Console.WriteLine($"Рандомное число: {number}");
+
+    result = FindCentralNumber(number);
+    Console.Write($"Вторая цифра числа {number} - {result}");
+} else if (number<100 || number>=1000)
+{
+    //Завершаем программу если пользователь ошибся
+    Console.Write("Вы ввели не трёхзначное число!");
+} else 
+{
+    result = FindCentralNumber(number);
+    Console.Write($"Вторая цифра числа {number} - {result}");
+}
+
+
+//Создаём метод по нахождению второго числа
+int FindCentralNumber(int num){
+    num = num / 10 % 10;
+    return num;
+}
---
+++ Lesson_2/007_Day_of_the_number/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+
+//Создаём переменную
+Console.Write("Введите любое число от 1 до 7: ");
+int week = Convert.ToInt32(Console.ReadLine());
+
+
+//Проверяем, является ли число днём недели и определяем по нему - выходной ли это день?
+Console.Write(week);
+switch (week){
+    case 1:
+        Console.Write(" - Рабочий день");
+        break;
+    case 2:
+        Console.Write(" - Рабочий день");
+        break;
+    case 3:
+        Console.Write(" - Рабочей недели");
+        break;
+    case 4:
+        Console.Write(" - Рабочий день");
+        break;
+    case 5:
+        Console.Write(" - Короткий рабочий день");
+        break;
+    case 6:
+        Console.Write(" - Выходной");
+        break;
+    case 7:
+        Console.Write(" - Выходной");
+        break;
+    default:
+        Console.Write(" - Не является числом обозначающий день недели!");
+        break;
+}
